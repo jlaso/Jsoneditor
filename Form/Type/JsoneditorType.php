91c8bf7f27aba523a61ee14f6a49665eae933397
joseluis laso <wld1373@gmail.com>
2013-03-26 09:35:46 +0100

    - Avances in Form Type

--- Form/Type/JsoneditorType.php
+++ Form/Type/JsoneditorType.php
@@ -6,6 +6,8 @@ namespace Jlaso\JsoneditorBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 
 class JsoneditorType extends AbstractType
 {
@@ -30,15 +32,24 @@ class JsoneditorType extends AbstractType
             'allow_add'         => true,
             'allow_delete'      => true,
             'by_reference'      => false,
-            'compound'          => true,
-            'expanded'          => true,
-            'label'             => false,
-            'label_attr'        => array('class' => 'hidden'),
-            'model_manager'     => null,
-            'modifiable'        => true,
-            'multiple'          => true,
-            'property'          => null,
-            'default'           => null
+            //'compound'          => true,
+            //'expanded'          => true,
+            //'label'             => false,
+            //'label_attr'        => array('class' => 'hidden'),
+            //'multiple'          => true,
+            //'property'          => null,
+            //'default'           => null
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'allow_add'    => $options['allow_add'],
+            'allow_delete' => $options['allow_delete'],
         ));
     }
 
